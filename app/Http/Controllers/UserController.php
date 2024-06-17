93453f69667fba95b6ea1a0efe309695250698b3
Stanley Jonathan Wahjudi <136320258+StanleyJo-37@users.noreply.github.com>
2024-06-17 20:11:41 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
     }
 
     public function loginUser(Request $request){
-        if(Auth::attempt(['email' => $request->email, 'password' => bcrypt($request->password)])){
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
             // $request->session()->regenerate();
             dd(Auth::user());
             return response()->json("Logged In");
